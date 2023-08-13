7af1ad520cc5194b435f47fc797aa579daf6b6dc
Konstantin Ivlev <ivlkostya@yandex.ru>
2023-08-14 08:52:01 +1000

    first commit

---
+++ DelitCross.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+#include <memory>
+
+class node
+{
+public:
+    int m_value;
+    std::weak_ptr<node> parent;
+    node(int value) : m_value{ value } {};
+    ~node() { std::cout << "destructor called\n"; }
+    /*std::unique_ptr<node> clone()
+    {
+        auto clone = std::make_unique<node>(m_value);
+        std::cout << "\nfunction clone was called\n";
+
+        return clone;*/
+    //};
+};
+
+int main()
+{
+    {
+        auto node1 = std::make_shared<node>(1);
+        auto node2 = std::make_shared<node>(2);
+        node1->parent = node2;
+        node2->parent = node1;
+    }
+
+    return 0;
+}
